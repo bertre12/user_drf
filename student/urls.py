8b438a3d019809bf615943943a06b36994bd5748
leonid <leoni@leonid>
2024-09-13 23:30:26 +0300

    Настройка библиотеки drf-spectacular и подключение endpoints.

--- student/urls.py
+++ student/urls.py
@@ -4,12 +4,12 @@ from student.views import CreateUserView, UserList, UserDelete, LoginView, \
     StudentUpdateView, StudentCreateView
 
 urlpatterns = [
-    path('create_user/', CreateUserView.as_view(), name='create_user'),
-    # Создание нового пользователя.
-    path('users/', UserList.as_view(), name='user_list'),  # Список
-    # пользователей.
-    path('users/<int:pk>/', UserDelete.as_view(), name='user_editing'),
-    # Редактирование и удаление пользователя по id.
+    # path('create_user/', CreateUserView.as_view(), name='create_user'),
+    # # Создание нового пользователя.
+    # path('users/', UserList.as_view(), name='user_list'),  # Список
+    # # пользователей.
+    # path('users/<int:pk>/', UserDelete.as_view(), name='user_editing'),
+    # # Редактирование и удаление пользователя по id.
     path('login/', LoginView.as_view(), name='login'),  # Вход в систему для
     # пользователей student из бд Student.
     path('students/', StudentCreateView.as_view(), name='create-student'),
